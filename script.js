4214670b24b02cc0287a12a556e1a67c482cda8a
Joshua <chojoshua99@gmail.com>
2022-01-07 16:52:04 -0800

    Worked out game logic

--- script.js
+++ script.js
@@ -1,35 +1,28 @@
-const gameBoard = (function(){
-    const _wrapper = document.querySelector(".wrapper");
-    //player factory
-    const Player = (decision, marker, turn, winner) => {
-        return{
-            decision,
-            marker,
-            turn,
-            winner
-        }
+//player factory
+const Player = (decision, marker, winner) => {
+    return{
+        decision,
+        marker,
+        winner,
     }
+}
 
+//game 'ref' factory
+const Referee = (turns, tie, winner, playerOneTurn) => {
 
-    //player objects
-    let playerOne = Player([], "O", true, false);
-    let playerTwo = Player([], "X", false, false);
+    return {
+        turns,
+        tie,
+        playerOneTurn,
+    }
+}
+
+const GAME_BOARD = (function(){
+    const _wrapper = document.querySelector(".wrapper");
 
     //a private grid size static
     const _gridSize = Array(9);
 
-    //ALL win combinations
-    const winCombo = 
-    [
-        [0,1,2],    
-        [3,4,5], 
-        [6,7,8],
-        [0,3,6], 
-        [1,4,7], 
-        [2,5,8],
-        [0,4,8], 
-        [2,4,6]
-    ];
 
     //grid variable 
     const grid = document.createElement("div");
@@ -50,106 +43,162 @@ const gameBoard = (function(){
     }
     makeGrid(grid);
 
+    const test = true;
+
+    console.log(PLAY_GAME)
+
+
     return {
-        playerOne,
-        playerTwo,
-        winCombo,
         grid,
+        test,
     }
-
-
 })();
 
-const playGame = (function(){
-    
-    /*
-        To play the game we need:
-            2 players
-            'referee' object
-            winning combinations
-    */
-    const grid = gameBoard.grid;
-    const playerOne = gameBoard.playerOne; //[], O, TRUE 
-    const playerTwo = gameBoard.playerTwo; //[], X, FALSE
-    const winCombo = gameBoard.winCombo;
-    //referee factory
-    const Referee = (turnCount, gameOver, winner) => {
-        return {
-            turnCount,
-            gameOver,
-        }
+const PLAY_GAME = (function(){
+    //ALL win combinations
+    const winCombo = 
+    [
+        [0,1,2],    
+        [3,4,5], 
+        [6,7,8],
+        [0,3,6], 
+        [1,4,7], 
+        [2,5,8],
+        [0,4,8], 
+        [2,4,6]
+    ];
+
+    const playerOne = Player([], "O", false);
+    const playerTwo = Player([], "X", false);
+    const referee = Referee(0, false, true); // turns tie playerOneTurn
+    const grid = GAME_BOARD.grid;
+    let availableCell = false;
+
+    //Player One Picks
+    //Board takes input, updates turn count
+    //Player Two Picks
+    //Board takes input, updates turn count
+    //Starting from turn 5, check for 3 in row
+    //if there is, referee declares winner
+    //else, tie (MAX_TURNS === 9)
+
+    function toggleTurn() {
+        (referee.playerOneTurn) ? referee.playerOneTurn = false : referee.playerOneTurn = true;
     }
 
-    //referee object
-    const gameRef = Referee(0, false, "");
-
-    function takeTurn (){
-        for(let i = 0; i< grid.children.length;i++){
-            grid.children[i].addEventListener("click", updateTurn);
-        }
+    function checkAvailable (cell) {
+        (cell.innerText !== "X" && cell.innerText !== "O") ? availableCell = true : availableCell = false;
     }
 
-    let updateTurn = (e) => {
-        //find whose turn it is
-        //apply the click
-        //update 'decision' in player variable
-        //go next turn
-        //count of total turns goes up
-        let cell = e.target;
-        let playerOneTurn = playerOne.turn;
-        console.log(playerOneTurn)
-        if(playerOneTurn === true){
-            if(cell.innerText !== "X" && cell.innerText !== "O"){
-                cell.innerText = "O";
-                playerOne.decision.push(e.target.className.substring(5));
-                playerOne.turn = false;
-                playerTwo.turn = true;
-                gameRef.turnCount++;
-            }
-        }
-        else if(playerOneTurn === false){
-            if(cell.innerText !== "X" && cell.innerText !== "O"){
-                cell.innerText = "X";
-                playerTwo.decision.push(e.target.className.substring(5));
-                playerTwo.turn = false;
-                playerOne.turn = true;
-                gameRef.turnCount++;
-            }
-        }
-        //check if anyone won
-        if(gameRef.turnCount > 4){
-            console.log(gameRef.turnCount)
-            checkScore(playerOne);
-            checkScore(playerTwo);
-        }
-    }
-        //use winCombo
-    let checkScore = (player) => {
-        let count = 0;
-        for(let i = 0; i < winCombo.length; i++){
-            for(let j = 0; j < winCombo[i].length; j++){
-                for(let k = 0; k < player.decision.length; k++){
-                    if(player.decision[k] == winCombo[i][j]){
-                        console.log(`test`)
-                        count++;
-                        break;
+    function checkWin(player) {
+        if(referee.turns > 4 && referee.tie === false){
+            let count = 0; //count to check 3-in-row
+
+            for(let i = 0; i < winCombo.length; i++){ //go through win combo array
+                for(let j = 0; j < winCombo[i].length; j++){ //in each element in each array in win combo
+                    for(let k = 0; k < player.decision.length; k++){ //compare with each element in decision
+                        if(player.decision[k] == winCombo[i][j]){
+                            count++;
+                        }
                     }
                 }
                 console.log(count);
                 if(count === 3){
-                    gameRef.gameOver = true; //the game is over
-                    player.winner = true; //find winner
-                    console.log(player);
-                    return;
+
+                    player.winner = true;
+                    return true;
+
                 }
+                count = 0;
             }
-            count = 0;
+        }
+
+        if(referee.turns === 9){
+            referee.tie = true;
         }
     }
 
+    //event listener on click -> Adds player's choice to player decisions array, marks the cell, and increases games total turns by 1
+    function takeTurn(){
+        for(let i=0; i<grid.children.length;i++){
+            let cell = grid.children[i];
+            cell.addEventListener("click", function(){
+                let index = cell.className.substring(5);
+                checkAvailable(cell);
+                toggleTurn();
+                console.log(referee.playerOneTurn)
+               if(referee.playerOneTurn === true && availableCell === true){
+                   referee.turns++;
+                   playerOne.decision.push(index);
+                   cell.innerText = playerOne.marker;
+                   checkWin(playerOne);
+                   endGame();
+               }
+               else if(referee.playerOneTurn === false && availableCell === true){
+                   referee.turns++;
+                   playerTwo.decision.push(index);
+                   cell.innerText = playerTwo.marker;
+                   checkWin(playerTwo);
+                   endGame();
+                   
+               }
+            })
+        }
+
+    }
+
+    function resetBoard() {
+        for(let i=0; i<grid.children.length; i++){
+            let cell = grid.children[i];
+            cell.innerText = i;
+        }
+        resetDefault(playerOne);
+        resetDefault(playerTwo);
+        refDefault(referee);
+        console.log(referee);
+    }
+
+    function resetDefault(player) {
+        player.winner = false;
+        player.decision = [];
+    }
+
+    function refDefault(referee){
+        referee.turns = 0;
+        referee.tie = false;
+        referee.playerOneTurn = false;
+    }
+
+    let btn = document.querySelector(".btncontainer");
+
+    btn.addEventListener("click", resetBoard);
+
+    function endGame() {
+        if(referee.tie === true){
+            //announce tie
+            console.log(`tie game`)
+            //reset game
+        }
+        if(playerOne.winner === true){
+            //anounce winner
+            console.log(`playerOne Wins!`)
+            //reset game
+        }
+
+
+        else if(playerTwo.winner === true){
+            //announce p2 wins
+            console.log(`player two wins!`)
+            //reset game
+            resetBoard();
+        }
+    }
     
-    takeTurn();
+    const getWinner = () => {
+        return availableCell;
+    }
 
-
-
+    return {
+        getWinner,
+    }
 })();
